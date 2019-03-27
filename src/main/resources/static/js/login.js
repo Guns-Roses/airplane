5d1ff9dec5d84d54adfcc3e6249dc442de1e9601
lvxutao <809283678@qq.com>
2019-03-27 09:35:53 +0800

    修改关于ajax提交进入error的问题

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -37,38 +37,3 @@ $(document).ready(function () {
     })
 
 })
-/*
-$(document).ready(function () {
-    $("#login-btn").click(function(){
-        var name=$("#name").val();
-        var pwd=$("#pwd").val();
-            if(userName!=""&&userPass!=""){
-                $.ajax({
-                    url:url+"/login",
-                    data:{
-                        username:userName,
-                        password:userPass
-                    },
-                    async:true,
-                    type:"POST",
-                    success:function(result){
-                        if(result.errCode==0){
-                            localStorage.tokenID=result.tokenID;
-                            localStorage.username=result.username;
-                            if(data.isManager == 1)
-                                window.location = "managerFlight.html";
-                            else if(data.isVip == 1) {
-                                window.location = "showFlight.html";
-                                alert("尊敬的会员，您可以享受八折优惠");
-                            }else {
-                                window.location = "showFlight.html";
-                            }
-                        }else{
-                            $("#tip").html("用户名或密码错误！")
-                        }
-                    }});
-            }else{
-                $("#tip").html("用户名和密码必须填写")
-            }
-    })
-})*/
