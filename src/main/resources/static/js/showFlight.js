8e6eb873f68f8519d17f288da7e7e2570a88496f
lvxutao <809283678@qq.com>
2019-03-29 18:41:24 +0800

    修改页面展示时间日期格式不正确的问题

--- src/main/resources/static/js/showFlight.js
+++ src/main/resources/static/js/showFlight.js
@@ -22,6 +22,39 @@ $(document).ready(function () {
         })
     })
 });
+/**
+ * 对Date的扩展，将 Date 转化为指定格式的String 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2
+ * 个占位符， 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 例子： (new
+ * Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 (new
+ * Date()).Format("yyyy-M-d h:m:s.S") ==> 2006-7-2 8:9:4.18
+ */
+Date.prototype.format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, // 月份
+        "d+": this.getDate(), // 日
+        "h+": this.getHours(), // 小时
+        "m+": this.getMinutes(), // 分
+        "s+": this.getSeconds(), // 秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+        "S": this.getMilliseconds() // 毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+function formatData(data){
+    for(i = 0; i < data.rows.length;i++){
+        data.rows[i].createTime = (new Date(data.rows[i].createTime)).format("yyyy-M-d hh:mm:ss");
+        data.rows[i].lastUpdateTime = (new Date(data.rows[i].lastUpdateTime)).format("yyyy-M-d hh:mm:ss");
+        if(data.rows[i].isEnable){
+            data.rows[i].isEnable = "是";
+        }else{
+            data.rows[i].isEnable = "否";
+        }
+    }
+    return data;
+}
 
 function show(data) {
     for (var i = 0; i < data.length; i++) {
@@ -34,7 +67,8 @@ function show(data) {
         //         "leftTicket": 50,
         //         "ticketPrice": 1000
         var startTTd = document.createElement('td');
-        startTTd.append(data[i].startTime);
+        startTTd.append(new Date(data[i].startTime).format("yyyy-MM-dd hh:mm:ss"));
+        //new Date(data[i].startTime).format("yyyy-MM-dd hh:mm:ss");
         var startCTd = document.createElement('td');
         startCTd.append(data[i].startCity);
         var endCTd = document.createElement('td');
