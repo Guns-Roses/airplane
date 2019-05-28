87688c4160983dac6ae126ca3e906c3a54a6f835
lvxutao <809283678@qq.com>
2019-05-28 17:08:41 +0800

    test

--- src/main/java/com/ticketsystem/controller/LoginController.java
+++ src/main/java/com/ticketsystem/controller/LoginController.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
-@Api(value = "登录注册功能实现", tags = "登录注册接口")
+@Api(value = "登录注册功能实现了", tags = "登录注册接口")
 @RestController
 @MapperScan("com.ticketsystem.dao")
 @RequestMapping("/user")
